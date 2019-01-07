78c2c41e0418bf3e0dbac3f8fad3870087876fe9
Uroš Zagoranski <uros.zagoranski@student.um.si>
2019-01-07 12:36:05 +0100

    SQL

--- SQL/ScrumTable.sql
+++ SQL/ScrumTable.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gostitelj: 127.0.0.1
--- Čas nastanka: 06. jan 2019 ob 15.35
+-- Čas nastanka: 07. jan 2019 ob 12.35
 -- Različica strežnika: 10.1.37-MariaDB
 -- Različica PHP: 7.2.12
 
@@ -21,6 +21,53 @@ SET time_zone = "+00:00";
 --
 -- Zbirka podatkov: `scrumtable`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `projekt`
+--
+
+CREATE TABLE `projekt` (
+  `idProjekt` int(11) NOT NULL,
+  `naziv` varchar(500) COLLATE utf8_slovenian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `sprint`
+--
+
+CREATE TABLE `sprint` (
+  `idSprint` int(11) NOT NULL,
+  `naziv` varchar(500) COLLATE utf8_slovenian_ci NOT NULL,
+  `idProjekt` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `task`
+--
+
+CREATE TABLE `task` (
+  `idTask` int(11) NOT NULL,
+  `naziv` varchar(500) COLLATE utf8_slovenian_ci NOT NULL,
+  `opis` varchar(500) COLLATE utf8_slovenian_ci NOT NULL,
+  `idSprint` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `tiptask`
+--
+
+CREATE TABLE `tiptask` (
+  `idTipTask` int(11) NOT NULL,
+  `naziv` varchar(50) COLLATE utf8_slovenian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
 
 -- --------------------------------------------------------
 
@@ -43,9 +90,49 @@ CREATE TABLE `uporabnik` (
 INSERT INTO `uporabnik` (`idUporabnik`, `ime`, `priimek`, `email`, `geslo`) VALUES
 (1, 'Test', 'Test', 'test@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
 
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `uporabnikhasprojekt`
+--
+
+CREATE TABLE `uporabnikhasprojekt` (
+  `idUporabnikHasProjekt` int(11) NOT NULL,
+  `idProjekt` int(11) NOT NULL,
+  `idUporabnik` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+
 --
 -- Indeksi zavrženih tabel
 --
+
+--
+-- Indeksi tabele `projekt`
+--
+ALTER TABLE `projekt`
+  ADD PRIMARY KEY (`idProjekt`),
+  ADD UNIQUE KEY `idProjekt` (`idProjekt`);
+
+--
+-- Indeksi tabele `sprint`
+--
+ALTER TABLE `sprint`
+  ADD PRIMARY KEY (`idSprint`),
+  ADD UNIQUE KEY `idSprint` (`idSprint`);
+
+--
+-- Indeksi tabele `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`idTask`),
+  ADD UNIQUE KEY `idTask` (`idTask`);
+
+--
+-- Indeksi tabele `tiptask`
+--
+ALTER TABLE `tiptask`
+  ADD PRIMARY KEY (`idTipTask`),
+  ADD UNIQUE KEY `idTipTask` (`idTipTask`);
 
 --
 -- Indeksi tabele `uporabnik`
@@ -55,14 +142,51 @@ ALTER TABLE `uporabnik`
   ADD UNIQUE KEY `idUporabnik` (`idUporabnik`);
 
 --
+-- Indeksi tabele `uporabnikhasprojekt`
+--
+ALTER TABLE `uporabnikhasprojekt`
+  ADD PRIMARY KEY (`idUporabnikHasProjekt`),
+  ADD UNIQUE KEY `idUporabnikHasProjekt` (`idUporabnikHasProjekt`);
+
+--
 -- AUTO_INCREMENT zavrženih tabel
 --
+
+--
+-- AUTO_INCREMENT tabele `projekt`
+--
+ALTER TABLE `projekt`
+  MODIFY `idProjekt` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT tabele `sprint`
+--
+ALTER TABLE `sprint`
+  MODIFY `idSprint` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT tabele `task`
+--
+ALTER TABLE `task`
+  MODIFY `idTask` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT tabele `tiptask`
+--
+ALTER TABLE `tiptask`
+  MODIFY `idTipTask` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT tabele `uporabnik`
 --
 ALTER TABLE `uporabnik`
   MODIFY `idUporabnik` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT tabele `uporabnikhasprojekt`
+--
+ALTER TABLE `uporabnikhasprojekt`
+  MODIFY `idUporabnikHasProjekt` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
